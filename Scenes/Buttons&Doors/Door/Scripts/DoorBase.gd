53b4350d68260da8b583021b049521d074b6ed19
Weaact <lucas.druckes@numericable.fr>
2019-12-19 17:38:09 +0100

    lever and doors

--- Scenes/Buttons&Doors/Door/Scripts/DoorBase.gd
+++ Scenes/Buttons&Doors/Door/Scripts/DoorBase.gd
@@ -1,10 +1,7 @@
-extends RigidBody2D
+extends StaticBody2D
 
 onready var animation_node = get_node("Animation")
 onready var collision_node = get_node("CollisionShape2D")
-
-func _ready():
-	pass # Replace with function body.
 
 func on_button_trigger():
 	animation_node.play("default")

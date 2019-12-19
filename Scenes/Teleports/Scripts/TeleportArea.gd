04697e788086c01d8bd9bbcd2acc5a13b4edb274
Hugo Baba <Hugobarette@gmail.com>
2019-12-19 05:33:19 +0100

    Fixing the teleport bug where you needed to go back and forth to teleport multiple time

--- Scenes/Teleports/Scripts/TeleportArea.gd
+++ Scenes/Teleports/Scripts/TeleportArea.gd
@@ -6,35 +6,45 @@ onready var layer_down_teleport_node: Node
 onready var players_nodes_array : Array = get_tree().get_nodes_in_group("Players")
 
 onready var teleport_master_node = get_parent()
+var teleporters_array : Array
 
 func _ready():
 	var _err
 	_err = connect("body_entered", self, "on_body_entered")
 	_err = connect("body_exited", self, "on_body_exited")
-	var children_array = teleport_master_node.get_children()
+	teleporters_array = teleport_master_node.get_children()
 	var i = get_index()
 	
 	var prec = i - 1
 	if prec < 0:
-		prec = len(children_array) - 1
+		prec = len(teleporters_array) - 1
 	var suiv = i + 1
-	if suiv > len(children_array) - 1:
+	if suiv > len(teleporters_array) - 1:
 		suiv = 0
 	
-	layer_up_teleport_node = children_array[prec]
-	layer_down_teleport_node = children_array[suiv]
+	layer_up_teleport_node = teleporters_array[prec]
+	layer_down_teleport_node = teleporters_array[suiv]
 
 
 # Whenever a character enters the area of this teleport, this method gives him the referecence to this node
 func on_body_entered(body):
-	if body in players_nodes_array:
+	if body in players_nodes_array and body.get_node("LayerChange").teleport_node != self:
 		body.get_node("LayerChange").teleport_node = self
 
 
 # Whenever a character exits the area of this teleport, set his teleport_node reference to null
 func on_body_exited(body):
-	if body in players_nodes_array and overlaps_body(body) == false:
-		body.get_node("LayerChange").teleport_node = null
+	if body in players_nodes_array:
+		var is_colliding = false
+		
+		# When the character get out of the area, check if it's because he left, or because he did teleport
+		for teleporter in teleporters_array:
+			if teleporter.overlaps_body(body) == true and teleporter != self:
+				is_colliding = true
+		
+		# If he left, set his teleport_node value to null
+		if is_colliding == false:
+			body.get_node("LayerChange").teleport_node = null
 
 
 # Teleport the player the the right destination teleporter

f018aa0eb75e7e86afaa4822f64d24d403171946
Hugo Baba <Hugobarette@gmail.com>
2020-01-16 18:59:51 +0100

    Add the countdown before gameover when a player exits the camera

---
+++ Scenes/OutsideScreenPopup/OutsideScreenPopup.gd
@@ -0,0 +1,33 @@
+extends TextureRect
+
+onready var timer_node : Timer = get_node("Timer")
+onready var label_node : Label = get_node("Label")
+
+var camera_node : Camera2D
+var player_node : Node
+
+signal gameover
+
+var offset := Vector2(8, 20)
+
+
+func _ready():
+	var _err
+	_err = timer_node.connect("timeout", self, "on_timer_timeout")
+	_err = camera_node.connect("player_inside_screen", self, "on_player_inside_screen")
+	timer_node.start()
+
+
+func _physics_process(_delta):
+	if player_node != null:
+		rect_position.y = player_node.position.y - offset.y
+		label_node.text = String(timer_node.get_time_left() as int)
+
+
+func on_timer_timeout():
+	emit_signal("gameover")
+
+
+func on_player_inside_screen(player):
+	if player == player_node:
+		queue_free()

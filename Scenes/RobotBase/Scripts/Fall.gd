ccab556a04ed1b2a48b542e368b85f515fa8d414
Hugo Baba <Hugobarette@gmail.com>
2020-01-16 12:14:13 +0100

    Change the method for triggering fall anim

--- Scenes/RobotBase/Scripts/Fall.gd
+++ Scenes/RobotBase/Scripts/Fall.gd
@@ -5,9 +5,33 @@ extends PlayerStateBase
 var state_node : Node
 var character_node : KinematicBody2D
 
+onready var fall_timer_node = get_node("FallTimer")
+var fall_timer_init_value : float
+
+
+func setup():
+	var _err = fall_timer_node.connect("timeout", self, "on_fall_timer_timeout")
+	fall_timer_init_value = fall_timer_node.get_wait_time()
+
+
 func update(_host, _delta):
 	if character_node.is_on_floor():
 		return "Idle"
 
+
+func enter_state(_host):
+	fall_timer_node.start()
+
+# Reset the falling timer cooldown, and stop it
+func exit_state(_host):
+	fall_timer_node.stop()
+	fall_timer_node.set_wait_time(fall_timer_init_value)
+
+
 func on_ActionPressed():
 	state_node.set_state("Action")
+
+
+# When the cooldown is over; play the animation
+func on_fall_timer_timeout():
+	animation_node.play(self.name)
--- Scenes/RobotBase/Scripts/Idle.gd
+++ Scenes/RobotBase/Scripts/Idle.gd
@@ -8,33 +8,22 @@ var layer_change_node : Node
 var character_node : KinematicBody2D
 var state_node : Node
 
-onready var fall_timer_node = get_node("FallTimer")
-var fall_timer_init_value : float
-
 
 # Setup method
 func setup():
 	var _err
 	_err = connect("layer_change", layer_change_node, "on_layer_change")
-	_err = fall_timer_node.connect("timeout", self, "on_fall_timer_timeout")
-	fall_timer_init_value = fall_timer_node.get_wait_time()
 
 
 # Check if the character is falling, with a small cooldown, before it triggers fall state
 func update(_host, _delta):
-	if !character_node.is_on_floor() && fall_timer_node.is_stopped():
-		fall_timer_node.start()
+	if !character_node.is_on_floor():
+		return "Fall"
 
 
 # Triggers the Idle aniamtion when entering the state
 func enter_state(_host):
 	animation_node.play(self.name)
-
-
-# Reset the falling timer cooldown, and stop it
-func exit_state(_host):
-	fall_timer_node.stop()
-	fall_timer_node.set_wait_time(fall_timer_init_value)
 
 
 func on_JumpPressed():
@@ -60,7 +49,3 @@ func on_LeftPressed():
 func on_RightPressed():
 	state_node.set_state("Move")
 
-
-# If the fall cooldown is over: change the state to False
-func on_fall_timer_timeout():
-	state_node.set_state("Fall")

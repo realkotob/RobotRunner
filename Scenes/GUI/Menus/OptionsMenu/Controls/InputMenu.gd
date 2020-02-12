5aad29905fe596e32ffae87d3ce92db3d7156583
Weaact <lucas.druckes@numericable.fr>
2020-02-12 09:24:25 +0100

    CDFGVHBJKL

--- Scenes/GUI/Menus/OptionsMenu/Controls/InputMenu.gd
+++ Scenes/GUI/Menus/OptionsMenu/Controls/InputMenu.gd
@@ -23,8 +23,8 @@ func rebuild(input_profile, is_customizable=false):
 func _on_InputLine_change_button_pressed(action_name, line):
 	set_process_input(false)
 	
-	$KeySelectMenu.open()
-	var key_scancode = yield($KeySelectMenu, "key_selected")
+	$CanvasLayer/KeySelectMenu.open()
+	var key_scancode = yield($CanvasLayer/KeySelectMenu, "key_selected")
 	$InputMapper.change_action_key(action_name, key_scancode)
 	line.update_key(key_scancode)
 	

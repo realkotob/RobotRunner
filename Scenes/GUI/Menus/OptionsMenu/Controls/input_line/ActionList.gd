434ba04a9a864b49d873206a8d473b5a8ab4f4ec
Hugo Baba <Hugobarette@gmail.com>
2020-03-18 09:52:33 +0100

    Correct Input menu graphics

--- Scenes/GUI/Menus/OptionsMenu/Controls/input_line/ActionList.gd
+++ Scenes/GUI/Menus/OptionsMenu/Controls/input_line/ActionList.gd
@@ -6,6 +6,7 @@ func clear():
 	for child in get_children():
 		child.free()
 
+
 func add_input_line(action_name, key, is_customizable=false):
 	var line = InputLine.instance()
 	line.initialize(action_name, key, is_customizable)	

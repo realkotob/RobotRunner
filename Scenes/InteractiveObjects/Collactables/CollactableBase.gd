142cee133b6d322baaae659da3541ef4e6b72f7c
Hugo Baba <Hugobarette@gmail.com>
2020-03-21 20:57:57 +0100

    Little ajustments of feedbacks

--- Scenes/InteractiveObjects/Collactables/CollactableBase.gd
+++ Scenes/InteractiveObjects/Collactables/CollactableBase.gd
@@ -4,17 +4,25 @@ class_name CollactableBase
 
 var aimed_character : Node
 
-export var speed : int = 20
+export var speed : int = 300
 
 var initial_velocity := Vector2.ZERO
 var velocity := Vector2.ZERO
 
+var initial_impulse : bool = true
+
 func _physics_process(_delta):
 	if aimed_character != null:
-		var direction = position.direction_to(aimed_character.position)
-		velocity += direction * speed
+		if initial_impulse == false:
+			var direction = position.direction_to(aimed_character.position)
+			velocity = direction * speed
+		else :
+			velocity = initial_velocity
 		
 		var _err = move_and_slide(velocity)
-		if position.distance_to(aimed_character.position) < 30:
-			queue_free()
+		if position.distance_to(aimed_character.position) < 10:
+			contact_with_player()
 
+
+func contact_with_player():
+	queue_free()
--- Scenes/InteractiveObjects/Collactables/XionCollectable.gd
+++ Scenes/InteractiveObjects/Collactables/XionCollectable.gd
@@ -1,9 +1,27 @@
 extends CollactableBase
 
+onready var timer_node = $Timer
+
+signal xion_received
+
 func _ready():
-	var random_ang = deg2rad(randi() % 360 + 1)
+	var _err = connect("xion_received", aimed_character, "on_xion_received")
+	_err = timer_node.connect("timeout", self, "on_timeout")
+	
+	# Genererate a random angle value between 0 and 359
+	var random_ang = deg2rad(randi() % 360)
 	var dir = Vector2(cos(random_ang), sin(random_ang))
 	
+	# Gennerate a random force value beteen 100 and 200
 	var random_force = randi() % 100 + 100
 	initial_velocity = dir * random_force
 	velocity = initial_velocity
+
+
+func on_timeout():
+	initial_impulse = false
+
+
+func contact_with_player():
+	emit_signal("xion_received")
+	queue_free()

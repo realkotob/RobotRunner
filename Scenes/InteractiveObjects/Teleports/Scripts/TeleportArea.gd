9e84ec96e8cd9333ee34afabf4caed802d026563
Weact <lucas.druckes@numericable.fr>
2020-04-02 21:23:40 +0200

    Level3A Finished - Rework later ? (please no)

--- Scenes/InteractiveObjects/Teleports/Scripts/TeleportArea.gd
+++ Scenes/InteractiveObjects/Teleports/Scripts/TeleportArea.gd
@@ -1,35 +1,36 @@
 extends Area2D
 
-onready var layer_up_teleport_node: Node
-onready var layer_down_teleport_node: Node
+onready var layer_change_teleport_node : Node
+
 onready var teleport_master_node = get_parent()
 
 var teleporters_array : Array
 var players_node_array : Array
+var player_already_on_teleportarea : bool
 
 func _ready():
 	add_to_group("InteractivesObjects")
 	var _err
 	_err = connect("body_entered", self, "on_body_entered")
 	_err = connect("body_exited", self, "on_body_exited")
-	teleporters_array = teleport_master_node.get_children()
-	var i = get_index()
 	
-	var prec = i - 1
-	if prec < 0:
-		prec = len(teleporters_array) - 1
-	var suiv = i + 1
+	#Get all the teleporters in the group
+	teleporters_array = teleport_master_node.get_children()
+	var teleport_index = get_index()
+	
+	#get the next teleportarea node in the teleporters_array array
+	var suiv = teleport_index + 1
+	#if the next teleporter's index is superior than the last index of the array, we set the index to 0 (first teleportarea in the array)
 	if suiv > len(teleporters_array) - 1:
 		suiv = 0
 	
-	layer_up_teleport_node = teleporters_array[prec]
-	layer_down_teleport_node = teleporters_array[suiv]
-
+	layer_change_teleport_node = teleporters_array[suiv]
 
 # Whenever a character enters the area of this teleport, this method gives him the referecence to this node
 func on_body_entered(body):
 	if body in players_node_array and body.get_node("LayerChange").teleport_node != self:
 		body.get_node("LayerChange").teleport_node = self
+		player_already_on_teleportarea = true
 
 
 # Whenever a character exits the area of this teleport, set his teleport_node reference to null
@@ -41,10 +42,12 @@ func on_body_exited(body):
 		for teleporter in teleporters_array:
 			if teleporter.overlaps_body(body) == true and teleporter != self:
 				is_colliding = true
+				player_already_on_teleportarea = false
 		
 		# If he left, set his teleport_node value to null
 		if is_colliding == false:
 			body.get_node("LayerChange").teleport_node = null
+			player_already_on_teleportarea = false
 
 
 # Teleport the player the the right destination teleporter
@@ -55,10 +58,8 @@ func teleport_layer(character : Node):
 	var teleport_pos : Vector2 = character.global_position
 	
 	# Check if one player is already on the teleport or not
-	if len(layer_up_teleport_node.get_overlapping_bodies()) <= 0 :
-		teleport_pos = layer_up_teleport_node.global_position
-	elif len(layer_down_teleport_node.get_overlapping_bodies()) <= 0:
-		teleport_pos = layer_down_teleport_node.global_position
+	if(!layer_change_teleport_node.player_already_on_teleportarea):
+		teleport_pos = layer_change_teleport_node.global_position
 	
-	teleport_pos.y -= y_offset
+	teleport_pos.y -= y_offset/2
 	character.set_position(teleport_pos)

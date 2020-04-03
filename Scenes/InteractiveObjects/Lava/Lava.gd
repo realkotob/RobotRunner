b4e9a323ab6e403b32e652c5b0f23641f33673da
Hugo Baba <Hugobarette@gmail.com>
2020-04-03 07:13:32 +0200

    Add Lava scene documentation

--- Scenes/InteractiveObjects/Lava/Lava.gd
+++ Scenes/InteractiveObjects/Lava/Lava.gd
@@ -1,9 +1,18 @@
 extends Area2D
 
+#### LAVA MAIN NODE ####
+
+# To instanciate a lave zone:
+# 1) Instanciate this scene
+# 2) Instanciate the scene LavaShader & a CollisionShape as children of it
+# 3) Instanciate the LavaLight as a child of the LavaShader
+# 4) Set the size of the texture, of the shape and of the light as you will
+
 func _ready():
 	var _err = connect("body_entered", self, "on_body_entered")
 
 
+# Destroy the body entering the lava
 func on_body_entered(body):
 	if body.is_class("Player"):
 		body.destroy()

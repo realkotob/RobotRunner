ac9870d4ec9ec4e670bd230cd67db05c2c19b8c0
Hugo Baba <Hugobarette@gmail.com>
2020-04-03 04:29:43 +0200

    Add the focus on door, or an average with players when the great doors open

--- Scenes/InteractiveObjects/GreatDoor/GreatDoor.gd
+++ Scenes/InteractiveObjects/GreatDoor/GreatDoor.gd
@@ -2,10 +2,16 @@ extends Door
 
 #### GREAT DOOR CLASS ####
 
+# When set to true (default), the camera will move until it reach the position of the door
+# When set to false, the camera will make an average between the average players position
+# And the door position, and move to it
+export var focus_on_door : bool = true
+
 func _ready():
 	animation_node.connect("animation_finished", self, "on_animation_finished")
 
 
+# Triggers the opening of the door
 func open_door():
 	if animation_node != null:
 		animation_node.play("Open")
@@ -16,9 +22,11 @@ func open_door():
 	if audio_node != null:
 		audio_node.play()
 	
-	GAME.move_camera_to(position, true)
+	# Triggers the camera movement
+	GAME.move_camera_to(position, !focus_on_door)
 
 
+# Reset the camera on follow state when the animation is over
 func on_animation_finished(anim_name: String):
 	if anim_name == "Open":
 		GAME.set_camera_on_follow()

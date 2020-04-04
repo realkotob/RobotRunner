936ef51204968b1ea9c8ec0ffe98b02d79e78aec
Hugo Baba <Hugobarette@gmail.com>
2020-04-04 14:22:00 +0200

    Basic adaptative music

--- Scenes/Levels/Base/LevelBase.gd
+++ Scenes/Levels/Base/LevelBase.gd
@@ -2,34 +2,49 @@ extends Node2D
 
 class_name LevelBase
 
-var players_array : Array
-
 onready var music_node = $Music
 onready var xion_cloud_node = get_node_or_null("XionCloud")
 
+onready var screen_size : Vector2 = get_viewport().get_size()
+onready var danger_distance : float = screen_size.x / 2
+
+var players_array : Array
+
 func _ready():
+	print(danger_distance)
 	instanciate_players()
-	music_node.play()
 
 
 func _physics_process(_delta):
-	pass
+	if xion_cloud_node == null:
+		return
+	
+	var closest_player = get_closest_player(xion_cloud_node)
+	
+	if closest_player == null:
+		return
+	
+	# Triggers the medium stream when one of the players is close enough from the cloud
+	if get_distance_to(closest_player, xion_cloud_node) <= danger_distance:
+		music_node.interpolate_stream_volume("Medium", 0.0)
+	else: # If the closest player is to far from the cloud, fade_out the medium stream
+		music_node.interpolate_stream_volume("Medium", -80.0)
 
 
 # Returns the distance between the two given elements
-func get_distance_from(element1: Node, element2: Node):
+func get_distance_to(element1: Node, element2: Node):
 	return element1.get_global_position().distance_to(element2.get_global_position())
 
 
 # Return the closest player from the element
 func get_closest_player(element: Node):
-	var smaller_distance := Vector2.INF
-	var current_distance := Vector2.ZERO
+	var smaller_distance : float = INF
+	var current_distance : float = 0.0
 	var closest_player : Node = null
 	
 	for player in players_array:
-		current_distance = get_distance_from(element, player)
-		if current_distance > smaller_distance:
+		current_distance = get_distance_to(element, player)
+		if current_distance < smaller_distance:
 			smaller_distance = current_distance
 			closest_player = player
 	
--- Scenes/Levels/Chapter1/Music/Music.gd
+++ Scenes/Levels/Chapter1/Music/Music.gd
@@ -2,6 +2,13 @@ extends Node
 
 onready var children_array = get_children()
 
+func _ready():
+	play()
+	
+	for child in children_array:
+		child.connect("finished", self, "loop_stream")
+
+
 # Start playing every layers of music
 func play():
 	for child in children_array:
@@ -21,3 +28,8 @@ func interpolate_stream_volume(stream_name : String, dest_volume: float):
 	if stream != null:
 		var current_volume = stream.get_volume_db()
 		stream.set_volume_db(lerp(current_volume, dest_volume, 0.05))
+
+
+# Restart every stream whenever one has finished playing 
+func loop_stream():
+	play()

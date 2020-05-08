56ff73c4f70e07970eb15f71eb54bca17ec7db0f
Hugo Baba <Hugobarette@gmail.com>
2020-05-08 14:45:51 +0200

    Jump buffer while falling

--- Scenes/Characters/RobotBase/Scripts/Fall.gd
+++ Scenes/Characters/RobotBase/Scripts/Fall.gd
@@ -4,23 +4,29 @@ signal layer_change
 
 ### FALL STATE ###
 
-onready var tolerence_timer_node = $Timer
+onready var tolerance_timer_node = $ToleranceTimer
+onready var jump_buffer_timer_node = $JumpBufferTimer
 
 var layer_change_node : Node
 var inputs_node : Node
 
-var jump_tolerence : bool = false
+var jump_tolerance : bool = false
+var jump_buffered : bool = false
 
 func setup():
 	var _err
 	_err = connect("layer_change", owner, "on_layer_change")
 	_err = animation_node.connect("animation_finished", self, "on_animation_finished")
-	_err = tolerence_timer_node.connect("timeout", self, "on_tolerence_timeout")
+	_err = tolerance_timer_node.connect("timeout", self, "on_tolerence_timeout")
+	_err = jump_buffer_timer_node.connect("timeout", self, "on_jump_buffer_timeout")
 
 
 func update(_host, _delta):
 	if owner.is_on_floor():
-		return "Idle"
+		if jump_buffered:
+			return "Jump"
+		else:
+			return "Idle"
 
 
 # Start the cool down at the entery of the state
@@ -30,14 +36,20 @@ func enter_state(_host):
 	
 	# Start the time during which the jump is tolerated
 	if previous_state == "Move" || previous_state == "Idle":
-		tolerence_timer_node.start()
-		jump_tolerence = true
+		tolerance_timer_node.start()
+		jump_tolerance = true
 	
 	# Triggers the StartFalling animation if it exists
 	if "StartFalling" in animation_node.get_sprite_frames().get_animation_names():
 		animation_node.play("StartFalling")
 	else:
 		animation_node.play(self.name)
+
+
+# Reset the jump tolerance and jump buffered bools to ensure no unwanted behaviours
+func _exit_state():
+	jump_tolerance = false
+	jump_buffered = false
 
 
 # Define the actions the player can do in this state
@@ -52,8 +64,11 @@ func _input(event):
 			
 			# The jump is tolerated only if the jump tolerence timer is still running
 			if event.is_action_pressed(inputs_node.input_map["Jump"]):
-				if jump_tolerence == true:
+				if jump_tolerance == true:
 					state_node.set_state("Jump")
+				else:
+					jump_buffer_timer_node.start()
+					jump_buffered = true
 
 
 # Triggers the fall animation when the start falling is over
@@ -63,6 +78,11 @@ func on_animation_finished():
 				animation_node.play(self.name)
 
 
-# When the tolerence time is finished
+# When the tolerence time is finished, reset the jump_tolerance to false
 func on_tolerence_timeout():
-	jump_tolerence = false
+	jump_tolerance = false
+
+
+# When the jump buffer time is finished, reset the jump_buffer to false
+func on_jump_buffer_timeout():
+	jump_buffered = false

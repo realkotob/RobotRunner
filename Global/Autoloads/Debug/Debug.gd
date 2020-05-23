6f8ca3df890664f663827510ed272cdabb5b377a
Hugo Baba <Hugobarette@gmail.com>
2020-05-23 17:09:27 +0200

    Add debug options

---
+++ Global/Autoloads/Debug/Debug.gd
@@ -0,0 +1,12 @@
+extends Node
+
+func _input(event):
+	if OS.is_debug_build():
+		if event.is_action_pressed("debug_fullscreen"):
+			OS.window_fullscreen = !OS.window_fullscreen
+			
+		
+		if event.is_action_pressed("debug_mute_sounds"):
+			var master_bus_id = AudioServer.get_bus_index("Master")
+			var is_master_muted = AudioServer.is_bus_mute(master_bus_id)
+			AudioServer.set_bus_mute(master_bus_id, !is_master_muted)

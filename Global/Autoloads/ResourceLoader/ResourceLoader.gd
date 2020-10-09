cc75f2846cde8e09bdbeecfac184627372f2474e
Hugo Baba <Hugobarette@gmail.com>
2020-10-09 07:25:46 +0200

    Remove debug from resource loader

--- Global/Autoloads/ResourceLoader/ResourceLoader.gd
+++ Global/Autoloads/ResourceLoader/ResourceLoader.gd
@@ -14,10 +14,11 @@ func _ready():
 	thread = Thread.new()
 	
 	var err = thread.start(self, "load_resources")
-	if err == ERR_CANT_CREATE:
-		print("Thread can't be created")
-	else:
-		print("Thread created sucesfully")
+	if debug:
+		if err == ERR_CANT_CREATE:
+			print("Thread can't be created")
+		else:
+			print("Thread created sucesfully")
 
 
 func _exit_tree():
@@ -32,8 +33,8 @@ func load_resources(_userdata):
 	for scene in target_array:
 		resource_array.append(load(scene))
 		yield(get_tree(), "idle_frame")
-	
-	print("Resource loading finished")
+	if debug:
+		print("Resource loading finished")
 
 
 #### VIRTUALS ####

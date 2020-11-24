ac0bd336fbf8efa2e7b09dbfdd1c22c1197ed13c
Hugo Baba <Hugobarette@gmail.com>
2020-11-24 14:47:16 +0100

    Empty ChucnkBin class

---
+++ Scenes/InfiniteMode/ChunckBin.gd
@@ -0,0 +1,33 @@
+extends Object
+class_name ChunckBin
+
+var bin_map : Array setget set_bin_map, get_bin_map
+var entry_exit_couple_array : Array = []
+
+
+#### ACCESSORS ####
+
+func is_class(value: String): return value == "" or .is_class(value)
+func get_class() -> String: return ""
+
+func set_bin_map(map: Array): bin_map = map
+func get_bin_map() -> Array: return bin_map
+
+#### BUILT-IN ####
+
+
+
+#### LOGIC ####
+
+
+
+
+#### VIRTUALS ####
+
+
+
+#### INPUTS ####
+
+
+
+#### SIGNAL RESPONSES ####

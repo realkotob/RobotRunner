81a67ddf4c7f77a670496ca5a57b9704f09c1a35
Hugo Baba <Hugobarette@gmail.com>
2020-12-13 22:03:46 +0100

    Camera anticipation offset

--- Scenes/Camera/States/Follow.gd
+++ Scenes/Camera/States/Follow.gd
@@ -1,7 +1,7 @@
 extends StateBase
 
-const ANTICIP_DIST : float = 100.0
-const ANTICIP_TRIGGER_RATIO : float = 0.15
+const ANTICIP_DIST : float = 170.0
+const ANTICIP_TRIGGER_RATIO : float = 0.3
 
 var camera : Camera2D
 var screen_size : Vector2
@@ -24,12 +24,15 @@ func update(_host, _delta):
 	# Move the camera speed towards the average postion
 	# With a horiziontal/vertical restriction
 	if len(players_array) > 0:
-	
+		
+		# Compute the dest of the camera
 		var average_pos : Vector2 = camera.compute_average_pos(players_array)
-	
+		var border_dir = get_border_approched(average_pos)
+		var dest = average_pos + border_dir * ANTICIP_DIST
+		
 		# Move the camera
-		if camera.get_global_position().distance_to(average_pos) > 3.0:
-			camera.start_moving(average_pos)
+		if camera.get_global_position().distance_to(dest) > 3.0:
+			camera.start_moving(dest)
 		
 		# Zoom/Dezoom if necesary
 		var max_dist = screen_size * 0.6
@@ -61,23 +64,88 @@ func compute_player_distance(players_array: Array) -> Vector2:
 	return distance
 
 
+## Retunrs the direction of the border to anticipate
+#func get_appropriate_border_direction(ply_average_pos: Vector2) -> Vector2:
+#	var borders_array = get_borders_approched(ply_average_pos)
+#	if borders_array.size() == 0: 
+#		return Vector2.ZERO
+#	elif borders_array.size() == 1: 
+#		return borders_array[0]
+#	else:
+#		var xion_cloud = get_tree().get_current_scene().find_node("XionCloud")
+#		if xion_cloud == null: return Vector2.RIGHT
+#
+#		var cloud_dir = xion_cloud.get_direction()
+#		if cloud_dir in borders_array:
+#			return borders_array[0]
+#		elif Vector2.RIGHT in borders_array : return Vector2.RIGHT
+#		else: return borders_array[0]
+
+### IDEA TO IMPROVE THE ANTICIPATION ALGORITME ###
+# 1) Check if at least one player is in the anticipation zone
+# 2) Find which player is the nearest form the border of the screen if it
+# wasn't zoomed and/or anticipated and returns its the nearest border
+
 # Returns an array of borders being approched
-func get_borders_approched() -> PoolVector2Array:
-	var borders := PoolVector2Array()
+func get_border_approched(ply_average_pos: Vector2) -> Vector2:
+	var border_player_couples := Array()
 	var players_array = owner.get_players_array()
 	
 	for player in players_array:
-		var player_pos = global_to_relative(player.get_global_position())
+		var player_pos = player.get_global_position()
 		
-		if player_pos > (screen_size / 2) - screen_size * ANTICIP_TRIGGER_RATIO && \
-		player.get_direction() == 1:
-			borders.append(Vector2.RIGHT)
+		if player_pos.x > ply_average_pos.x + (screen_size.x / 2) - screen_size.x * ANTICIP_TRIGGER_RATIO && \
+		player.last_direction == 1:
+			border_player_couples.append([Vector2.RIGHT, player])
 		
-		elif player_pos < -(screen_size / 2) + screen_size * ANTICIP_TRIGGER_RATIO && \
-		player.get_direction() == -1:
-			borders.append(Vector2.LEFT)
+		elif player_pos.y < ply_average_pos.y - (screen_size.y / 2) + screen_size.y * ANTICIP_TRIGGER_RATIO && \
+		player.last_direction == -1:
+			border_player_couples.append([Vector2.LEFT, player])
 	
-	return borders
+	if border_player_couples.size() == 0: return Vector2.ZERO
+	elif border_player_couples.size() == 1: return border_player_couples[0][0]
+	else: return find_nearest_border(border_player_couples, ply_average_pos)
+
+
+# Find the direction of the nearest border of the theorical screen
+# (If the camera wasn't dezoomed of anticipated) form the average player position
+func find_nearest_border(couples_array: Array, play_avg_pos: Vector2) -> Vector2:
+	var dist_array := Array()
+	for couple in couples_array:
+		var player = couple[1]
+		var border_dir = couple[0]
+		var player_pos = player.get_global_position()
+		
+		if is_pos_outside_theorical_screen(player_pos):
+			return border_dir
+		
+		var border_pos = play_avg_pos.x + (screen_size.x / 2)
+		if border_dir == Vector2.RIGHT:
+			border_pos = play_avg_pos.x - (screen_size.x / 2)
+		
+		dist_array.append(abs(player_pos - border_pos))
+	
+	var nearest_border_id = find_smallest_value_id(dist_array)
+	return couples_array[nearest_border_id][0]
+
+
+# Retrun true if the given pos is outside the theorical space of the screen
+# If the camera had no zoom and no anticipation offset
+func is_pos_outside_theorical_screen(pos: Player) -> bool:
+	var avg_pos = camera.compute_average_pos()
+	return pos.x > avg_pos.x + screen_size.x / 2 or pos.x < avg_pos.x - screen_size.x / 2 or \
+		pos.y > avg_pos.y + screen_size.y / 2 or pos.y < avg_pos.y - screen_size.y / 2
+
+
+# Returns the id of the smallest value in the array
+func find_smallest_value_id(array: Array) -> int:
+	var smallest_value : float = INF
+	var smallest_value_id : int = -1
+	for i in range(array.size()):
+		if array[i] < smallest_value:
+			smallest_value = array[i]
+			smallest_value_id = i
+	return smallest_value_id
 
 
 # Convert a global position to a relative position to the camera origin (center)

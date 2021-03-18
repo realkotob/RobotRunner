c9e0e89837870e4b9c57c67c85517746109a3a36
Hugo Baba <Hugobarette@gmail.com>
2021-03-18 18:02:20 +0100

    level binds crooked

--- Scenes/WorldMap/Bind/LevelNodeBind.gd
+++ Scenes/WorldMap/Bind/LevelNodeBind.gd
@@ -10,6 +10,9 @@ export var destination_node_path : String = ""
 var origin : Node2D setget set_origin, get_origin
 var destination : Node2D setget set_destination, get_destination
 
+var origin_pos := Vector2.INF setget set_origin_pos
+var dest_pos := Vector2.INF setget set_dest_pos
+
 var is_ready : bool = false
 
 #### ACCESSORS ####
@@ -23,6 +26,9 @@ func set_origin(value: Node2D):
 	origin = value
 	if origin_node_path == "" && owner != null:
 		origin_node_path = owner.get_path_to(origin)
+	
+	if origin == null:
+		origin_pos == Vector2.INF
 
 func get_origin() -> Node2D: return origin
 
@@ -32,9 +38,21 @@ func set_destination(value: Node2D):
 	destination = value
 	if destination_node_path == "" && owner != null:
 		destination_node_path = owner.get_path_to(destination)
+	
+	if destination == null:
+		dest_pos == Vector2.INF
 
 func get_destination() -> Node2D: return destination
 
+func set_origin_pos(value: Vector2): 
+	if value != origin_pos:
+		origin_pos = value
+		_update_line()
+
+func set_dest_pos(value: Vector2): 
+	if value != dest_pos:
+		dest_pos = value
+		_update_line()
 
 #### BUILT-IN ####
 
@@ -49,7 +67,10 @@ func _ready() -> void:
 
 
 func _process(delta: float) -> void:
-	_update_line()
+	if Engine.editor_hint:
+		if origin != null && destination != null:
+			set_origin_pos(origin.get_global_position())
+			set_dest_pos(destination.get_global_position())
 
 #### VIRTUALS ####
 
@@ -59,13 +80,23 @@ func _process(delta: float) -> void:
 
 
 func _update_line():
-	if origin == null or destination == null:
+	if Vector2.INF in [origin_pos, dest_pos]:
 		return
 	
 	curve.clear_points()
 	
-	curve.add_point(origin.get_global_position())
-	curve.add_point(destination.get_global_position())
+	curve.add_point(origin_pos)
+	
+	var x_dist = abs(origin_pos.x - dest_pos.x)
+	var y_dist = abs(origin_pos.y - dest_pos.y)
+	
+	if x_dist != 0.0 && y_dist != 0.0:
+		if x_dist > y_dist:
+			curve.add_point(Vector2(origin_pos.x, dest_pos.y))
+		else:
+			curve.add_point(Vector2(dest_pos.x, origin_pos.y))
+	
+	curve.add_point(dest_pos)
 	
 	var points = curve.get_baked_points()
 	line.set_points(points)

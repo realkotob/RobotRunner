3596474b4f9baa4094432cfd853daea8292764bb
Hugo Baba <Hugobarette@gmail.com>
2021-03-19 18:01:45 +0100

    Add cursor feedback

--- Scenes/WorldMap/WorldMapMovingElement/Cursor/WorldMapCursor.gd
+++ Scenes/WorldMap/WorldMapMovingElement/Cursor/WorldMapCursor.gd
@@ -1,6 +1,14 @@
 extends WorldMapMovingElement
 class_name WorldMapCursor
 
+# warnings-disable
+
+onready var sprite_container = $SpriteContainer
+
+signal idle_animation_finished
+signal rotation_animation_finished
+
+
 #### ACCESSORS ####
 
 func is_class(value: String): return value == "WorldMapCursor" or .is_class(value)
@@ -9,7 +17,8 @@ func get_class() -> String: return "WorldMapCursor"
 
 #### BUILT-IN ####
 
-
+func _ready() -> void:
+	var __ = connect("path_finished", self, "_on_path_finished")
 
 #### VIRTUALS ####
 
@@ -17,6 +26,18 @@ func get_class() -> String: return "WorldMapCursor"
 
 #### LOGIC ####
 
+func move_to_level(level_node: LevelNode):
+	var current_state_name = $StatesMachine.get_state().name
+	
+	if current_state_name == "Move":
+		return
+	
+	$StatesMachine.set_state("Move")
+	match(current_state_name):
+		"Idle": yield(self, "idle_animation_finished")
+		"Rotate": yield(self, "rotation_animation_finished")
+	
+	.move_to_level(level_node)
 
 
 #### INPUTS ####
@@ -24,3 +45,6 @@ func get_class() -> String: return "WorldMapCursor"
 
 
 #### SIGNAL RESPONSES ####
+
+func _on_path_finished():
+	$StatesMachine.set_state("Idle")

45c5215749ab0b2bf049a380e6d4f081b49cabf7
Weact <l.druckes@ludus-academie.com>
2021-04-07 10:05:47 +0200

    moving platform

--- Scenes/Actor/Platforms/Moving/MovingPlatform.gd
+++ Scenes/Actor/Platforms/Moving/MovingPlatform.gd
@@ -5,10 +5,21 @@ class_name MovingPlatform
 func is_class(value: String): return value == "MovingPlatform" or .is_class(value)
 func get_class() -> String: return "MovingPlatform"
 
+onready var platform_start_waypoint_node = $PathWaypoints/StartWaypoint
+onready var platform_end_waypoint_node = $PathWaypoints/EndWaypoint
+
+export var accel : float = 0.0
+export var deccel : float = 0.0
+export var speed : float = 0.0
+
+var start_waypoint_coordinates : Vector2
+var end_waypoint_coordinates : Vector2
 
 #### BUILT-IN ####
 
-
+func _ready():
+	start_waypoint_coordinates = platform_start_waypoint_node.transform
+	end_waypoint_coordinates = platform_end_waypoint_node.transform
 
 #### VIRTUALS ####
 
